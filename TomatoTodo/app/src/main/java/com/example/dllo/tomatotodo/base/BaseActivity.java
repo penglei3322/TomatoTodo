a82188a60b387cb2fb90a58a96165bd8e719c299
彭磊 <337457103@qq.com>
2016-07-16 14:23:14 +0800

    1

--- TomatoTodo/app/src/main/java/com/example/dllo/tomatotodo/base/BaseActivity.java
+++ TomatoTodo/app/src/main/java/com/example/dllo/tomatotodo/base/BaseActivity.java
@@ -3,14 +3,15 @@ package com.example.dllo.tomatotodo.base;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by dllo on 16/7/16.
  */
-public abstract class BaseActivity extends Activity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     public abstract int initView();
-    
+
     public abstract void initData();
 
     @Override
