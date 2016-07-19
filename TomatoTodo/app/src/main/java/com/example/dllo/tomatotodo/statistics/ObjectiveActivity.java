f2137e13923cee7eef511b13d4f89468e36116ac
彭磊 <337457103@qq.com>
2016-07-19 20:39:20 +0800

    1111

---
+++ TomatoTodo/app/src/main/java/com/example/dllo/tomatotodo/statistics/ObjectiveActivity.java
@@ -0,0 +1,18 @@
+package com.example.dllo.tomatotodo.statistics;
+
+import com.example.dllo.tomatotodo.base.BaseActivity;
+
+/**
+ * Created by dllo on 16/7/19.
+ */
+public class ObjectiveActivity extends BaseActivity{
+    @Override
+    public int initView() {
+        return 0;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
